e06acdfab06335abdf4f755e3de97de93712947b
h1261 <h1261109615@qq.com>
2019-01-02 15:38:23 +0800

    1.eureka集群

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# cloudshop
-SpringCloud的G版本应用
+#  SpringCloud的G版本应用
+**一.cloud-eureka-server eureka注册中心集群**
+1. 这是使用的是伪集群，节点peer1，peer2，peer3都是映射IP本地地址
+2. 利用IDEA启动多个实例来启动注册中心集群
+
+**二.**
+
---
+++ cloud-service-provider/src/main/java/com/huangsm/cloud/cloudeurekaserver/Application.java
@@ -0,0 +1,14 @@
+package com.huangsm.cloud.cloudeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
